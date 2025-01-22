05db17d2490b43ba878b81c38eedc1d7ff5c6423
Kishor-Kumar47 <ksir3334445@gmail>
2025-01-22 14:13:00 +0500

    new update clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
-import { ClerkProvider } from '@clerk/nextjs';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <ClerkProvider>
+      
 
     <html lang="en">
       <body className={inter.className}>
@@ -27,6 +27,6 @@ export default function RootLayout({
         </body>
     </html>
       
-   </ClerkProvider>
+  
   );
 }
